3534828c9181cec9eca6ee89903614a3670cc8e1
Silva G H E H <132427118+hehsilva@users.noreply.github.com>
2023-07-31 19:23:18 +0530

    upload java labsheet 1

---
+++ lab sheet 1/items/src/main/java/com/mycompany/items/Monster.java
@@ -0,0 +1,9 @@
+package com.mycompany.items;
+public class Monster extends Item {
+    
+    public Monster(int location, String description) {
+        
+        super(location, description);
+    }
+}
+

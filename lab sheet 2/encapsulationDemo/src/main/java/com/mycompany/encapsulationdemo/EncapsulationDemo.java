bba9682d5defe107e1798233de1ee5cf8058f4a0
Silva G H E H <132427118+hehsilva@users.noreply.github.com>
2023-07-31 19:27:23 +0530

    upload java labsheet 2

---
+++ lab sheet 2/encapsulationDemo/src/main/java/com/mycompany/encapsulationdemo/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package com.mycompany.encapsulationdemo;
+public class EncapsulationDemo{
+    private String empName;
+    public String getEmpName(){
+        return empName;
+    }
+
+    public void setEmpName(String newValue){
+        empName = newValue;
+    }
+
+}
+
